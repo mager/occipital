23cb848b1e6c40b2768b9d509281d9787a2e5c5b
Mager <andrew.mager@gmail.com>
2024-12-28 13:12:06 -0600

    Handle Discover

--- handler/discover/discover.go
+++ handler/discover/discover.go
@@ -12,8 +12,8 @@ import (
 	"github.com/mager/occipital/occipital"
 	"github.com/mager/occipital/spotify"
 	"go.uber.org/zap"
-	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // DiscoverHandler is an http.Handler
@@ -40,18 +40,34 @@ type DiscoverRequest struct {
 }
 
 type DiscoverResponse struct {
-	Tracks []occipital.Track `json:"tracks"`
+	Tracks    []occipital.Track `json:"tracks"`
+	Billboard []occipital.Track `json:"billboard"`
+	HypeM     []occipital.Track `json:"hypem"`
 }
 
 // Function to convert fsClient.Track to occipital.Track
-func convertToOccipitalTrack(fsTrack fsClient.Track) occipital.Track {
+func convertToOccipitalTrack(fsTrack fsClient.Track, thumbType string) occipital.Track {
+	var image string
+	if thumbType == "hypem" {
+		image = getHypemThumb(fsTrack.Thumb)
+	} else {
+		image = getSpotifyThumb(fsTrack.Thumb)
+	}
 	return occipital.Track{
 		Artist:   fsTrack.Artist,
 		Name:     fsTrack.Title,
 		Source:   "SPOTIFY",
 		SourceID: fsTrack.SpotifyID,
-		Image:    fmt.Sprintf("https://i.scdn.co/image/%s", fsTrack.Thumb),
+		Image:    image,
 	}
+}
+
+func getSpotifyThumb(th string) string {
+	return fmt.Sprintf("https://i.scdn.co/image/%s", th)
+}
+
+func getHypemThumb(th string) string {
+	return fmt.Sprintf("https://static.hypem.com/items_images/%s", th)
 }
 
 // Discover
@@ -64,25 +80,24 @@ func convertToOccipitalTrack(fsTrack fsClient.Track) occipital.Track {
 func (h *DiscoverHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	ctx := context.Background()
 	w.Header().Set("Content-Type", "application/json")
-	h.log.Debug("Received request", zap.String("method", r.Method), zap.String("url", r.URL.String())) // Debug log for incoming request
 
 	var req DiscoverRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-		h.log.Error("Error decoding request body", zap.Error(err)) // Log error if decoding fails
+		h.log.Error("Error decoding request body", zap.Error(err))
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	now := time.Now()
-	offset := (int(now.Weekday()) + 4) % 7 // 4 is the offset to get to Wednesday
-	wednesday := now.AddDate(0, 0, -offset).Format("2006-01-02")
+	today := now.Format("2006-01-02")
+	yesterday := now.AddDate(0, 0, -1).Format("2006-01-02")
 
-	docsnap, err := h.fs.Collection("billboard").Doc(wednesday).Get(ctx)
+	// Fetch Billboard
+	billboardDoc, err := h.fs.Collection("billboard").Doc(today).Get(ctx)
 	if err != nil {
-		if grpc.Code(err) == codes.NotFound { // Check if the error is a "not found" error
-			h.log.Warn("Document not found for current Wednesday, searching for last Wednesday") // Log warning
-			lastWednesday := now.AddDate(0, 0, -7).AddDate(0, 0, -offset).Format("2006-01-02")   // Calculate last Wednesday
-			docsnap, err = h.fs.Collection("billboard").Doc(lastWednesday).Get(ctx)              // Attempt to fetch last Wednesday
+		if status.Code(err) == codes.NotFound {
+			h.log.Warn("Document not found for today, searching for yesterday")
+			billboardDoc, err = h.fs.Collection("billboard").Doc(yesterday).Get(ctx)
 		}
 		if err != nil {
 			h.log.Error("Error fetching document snapshot", zap.Error(err))
@@ -90,19 +105,47 @@ func (h *DiscoverHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
-
-	var tracksDoc fsClient.TracksDoc
-	if err := docsnap.DataTo(&tracksDoc); err != nil {
-		h.log.Error("Error converting document snapshot to tracks", zap.Error(err)) // Log error if conversion fails
+	var billboardTracksDoc fsClient.TracksDoc
+	if err := billboardDoc.DataTo(&billboardTracksDoc); err != nil {
+		h.log.Error("Error converting document snapshot to tracks", zap.Error(err))
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	// Map fsTracks to occipital.Tracks
-	var tracks []occipital.Track
-	for _, fsTrack := range tracksDoc.Tracks {
-		tracks = append(tracks, convertToOccipitalTrack(fsTrack))
+	var billboardTracks []occipital.Track
+	for _, fsTrack := range billboardTracksDoc.Tracks {
+		billboardTracks = append(billboardTracks, convertToOccipitalTrack(fsTrack, "spotify"))
 	}
 
-	json.NewEncoder(w).Encode(tracks)
+	// Fetch Hype Machine
+	hypemDoc, err := h.fs.Collection("hypem").Doc(today).Get(ctx)
+	if err != nil {
+		if status.Code(err) == codes.NotFound {
+			h.log.Warn("Document not found for today, searching for yesterday")
+			hypemDoc, err = h.fs.Collection("billboard").Doc(yesterday).Get(ctx)
+		}
+		if err != nil {
+			h.log.Error("Error fetching document snapshot", zap.Error(err))
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+	var hypemTracksDoc fsClient.TracksDoc
+	if err := hypemDoc.DataTo(&hypemTracksDoc); err != nil {
+		h.log.Error("Error converting document snapshot to tracks", zap.Error(err))
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var hypemTracks []occipital.Track
+	for _, fsTrack := range hypemTracksDoc.Tracks {
+		billboardTracks = append(billboardTracks, convertToOccipitalTrack(fsTrack, "hypem"))
+	}
+
+	resp := &DiscoverResponse{
+		Billboard: billboardTracks,
+		HypeM:     hypemTracks,
+	}
+
+	json.NewEncoder(w).Encode(resp)
 }
